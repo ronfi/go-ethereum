1147506fed11b1c67ddfd673bb8e326f3b2831e2
byhashdong <byhashdong@protonmail.com>
2023-07-25 09:29:06 +0800

    update v2/v3 valid factory map

--- ronfi/common/valid_v2facts.go
+++ ronfi/common/valid_v2facts.go
@@ -3,7 +3,32 @@ package common
 import "github.com/ethereum/go-ethereum/common"
 
 var (
-	ValidV2FactsMap = map[common.Address]string{
-		common.HexToAddress("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"): "uniswap v2",
+	ValidV2FactsMap = map[common.Address]int{
+		common.HexToAddress("0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"): 30,
+		common.HexToAddress("0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac"): 30,
+		common.HexToAddress("0x1e895bFe59E3A5103e8B7dA3897d1F2391476f3c"): 100,
+		common.HexToAddress("0x115934131916C8b277DD010Ee02de363c09d037c"): 30,
+		common.HexToAddress("0x9DEB29c9a4c7A88a3C0257393b7f3335338D9A9D"): 30,
+		common.HexToAddress("0x1097053Fd2ea711dad45caCcc45EfF7548fCB362"): 25,
+		common.HexToAddress("0x0388C1E0f210AbAe597B7DE712B9510C6C36C857"): 30,
+		common.HexToAddress("0x35113a300ca0D7621374890ABFEAC30E88f214b1"): 20,
+		common.HexToAddress("0xee3E9E46E34a27dC755a63e2849C9913Ee1A06E2"): 30,
+		common.HexToAddress("0x5D8d3bbE66076e844C82B989F8430Fa92Cf67DBA"): 30,
+		common.HexToAddress("0x43eC799eAdd63848443E2347C49f5f52e8Fe0F6f"): 100,
+		common.HexToAddress("0x5FA0060FcfEa35B31F7A5f6025F0fF399b98Edf1"): 30,
+		common.HexToAddress("0x4eef5746ED22A2fD368629C1852365bf5dcb79f1"): 30,
+		common.HexToAddress("0xa206F6274B178b5090F64f39CA49878955D93756"): 30,
+		common.HexToAddress("0x7F09d4bE6bbF4b0fF0C97ca5c486a166198aEAeE"): 20,
+		common.HexToAddress("0x75e48C954594d64ef9613AeEF97Ad85370F13807"): 30,
+		common.HexToAddress("0x03407772F5EBFB9B10Df007A2DD6FFf4EdE47B53"): 30,
+		common.HexToAddress("0x26F53fbADeEb777fb2A122dC703433d79241b64e"): 30,
+		common.HexToAddress("0x773CaDC167DEAfa46F603d96172fa45686c4FA58"): 30,
+		common.HexToAddress("0x6C565c5bbDc7F023Cae8a2495105A531caac6E54"): 70,
+		common.HexToAddress("0x8a93B6865C4492fF17252219B87eA6920848EdC0"): 30,
+		common.HexToAddress("0xa7028337D3DA1F04d638Cc3B4dD09411486b49EA"): 30,
+		common.HexToAddress("0xf5d8E98ef1f66f1cAb949A2555837FE755fc2D68"): 30,
+		common.HexToAddress("0x460b2005b3318982feADA99f7ebF13e1D6f6eFfE"): 25,
+		common.HexToAddress("0xd87Ad19db2c4cCbf897106dE034D52e3DD90ea60"): 30,
+		common.HexToAddress("0x91fAe1bc94A9793708fbc66aDcb59087C46dEe10"): 30,
 	}
 )
--- ronfi/common/valid_v3facts.go
+++ ronfi/common/valid_v3facts.go
@@ -6,5 +6,6 @@ var (
 	// ValidV3FactsMap is a list of valid v3 pools
 	ValidV3FactsMap = map[common.Address]string{
 		common.HexToAddress("0x1F98431c8aD98523631AE4a59f267346ea31F984"): "uniswap v3",
+		common.HexToAddress("0xbACEB8eC6b9355Dfc0269C18bac9d6E2Bdc29C4F"): "sushiswap v3",
 	}
 )
