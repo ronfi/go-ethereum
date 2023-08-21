fb2a35bb3db03fe1894b29d66b4b2c1a2b2f7614
byhashdong <byhashdong@protonmail.com>
2023-08-21 17:03:09 +0800

    adapt flashbot for sandwich

--- ronfi/trading/falshbot.go
+++ ronfi/trading/falshbot.go
@@ -63,3 +63,58 @@ func simulateFlashbotBundle(dexTxHex, arbTxHex string, expectedBlock uint64) {
 		log.Info("RonFi Flashbot", "response", result)
 	}
 }
+
+func FlashbotSandWich(flashRpc *flashbotsrpc.BuilderBroadcastRPC, block *types.Block, currentBlock uint64, txs []*types.Transaction) {
+	aLegTxBytes, err := rlp.EncodeToBytes(txs[0])
+	if err != nil {
+		log.Warn("RonFi Flashbot encode aLegTx failed", "err", err)
+	}
+	aLegTxHex := fmt.Sprintf("0x%x", aLegTxBytes)
+
+	targetTx, err := txs[1].MarshalBinary()
+	if err != nil {
+		log.Warn("RonFi Flashbot encode targetTx failed", "err", err)
+	}
+	targetTxHex := fmt.Sprintf("0x%x", targetTx)
+
+	bLegTxBytes, err := rlp.EncodeToBytes(txs[2])
+	if err != nil {
+		log.Warn("RonFi Flashbot encode bLegTx failed", "err", err)
+	}
+	bLegTxHex := fmt.Sprintf("0x%x", bLegTxBytes)
+
+	expectedBlock := currentBlock + 1
+
+	simulateFlashbotSandWichBundle(aLegTxHex, targetTxHex, bLegTxHex, expectedBlock)
+
+	sendBundleArgs := flashbotsrpc.FlashbotsSendBundleRequest{
+		Txs:         []string{aLegTxHex, targetTxHex, bLegTxHex},
+		BlockNumber: fmt.Sprintf("0x%x", expectedBlock),
+	}
+
+	results := flashRpc.BroadcastBundle(privateKey, sendBundleArgs)
+	for i, result := range results {
+		if result.Err != nil {
+			log.Warn("RonFi FlashbotSandWich broadcast failed", "tx", txs[i].Hash().String(), "err", result.Err)
+		} else {
+			log.Info("RonFi FlashbotSandWich", "response", result.BundleResponse.BundleHash)
+		}
+	}
+}
+
+func simulateFlashbotSandWichBundle(aLegTxHex, targetTxHex, bLegTxHex string, expectedBlock uint64) {
+	// simulate
+	flashBotRpc := flashbotsrpc.NewFlashbotsRPC("https://relay.flashbots.net")
+
+	callBundleArgs := flashbotsrpc.FlashbotsCallBundleParam{
+		Txs:              []string{aLegTxHex, targetTxHex, bLegTxHex},
+		BlockNumber:      fmt.Sprintf("0x%x", expectedBlock),
+		StateBlockNumber: "latest",
+	}
+	result, err := flashBotRpc.FlashbotsCallBundle(privateKey, callBundleArgs)
+	if err != nil {
+		log.Warn("RonFi simulateFlashbotSandWichBundle call failed", "err", err)
+	} else {
+		log.Info("RonFi simulateFlashbotSandWichBundle", "response", result)
+	}
+}
--- ronfi/trading/worker.go
+++ ronfi/trading/worker.go
@@ -711,7 +711,7 @@ func (w *Worker) sandwichTx(tx *types.Transaction, pairsInfo []*defi.SwapPairInf
 	}
 	if txs, ok := ronSandWich.Build(); ok && len(txs) >= 3 {
 		log.Info("RonFi sandwichTx", "tx", tx.Hash().String(), "elapsed", mclock.Since(handlerStartTime))
-		Flashbot(w.flashRpc, w.currentBlock, w.currentBlockNum, txs)
+		FlashbotSandWich(w.flashRpc, w.currentBlock, w.currentBlockNum, txs)
 	}
 }
 
